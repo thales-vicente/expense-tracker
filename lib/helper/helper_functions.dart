d494b385ddebbaffde85dba86fd057963d0fd43d
Thales <thalesvicentebem10@hotmail.com>
2025-05-20 11:23:16 -0300

    Add helper functions for currency formatting

--- lib/helper/helper_functions.dart
+++ lib/helper/helper_functions.dart
@@ -3,7 +3,15 @@ These are some helpful functions used across the app
  */
 
 // TODO convert string to a double
+import 'package:intl/intl.dart';
+
 double convertStringToDouble(String string){
   double? amount = double.tryParse(string);
   return amount ?? 0;
+}
+
+// TODO format double into dollars & cents
+String formatAmount(double amount) {
+  final format = NumberFormat.currency(locale: "en_US", symbol: "\$", decimalDigits: 2);
+  return format.format(amount);
 }
