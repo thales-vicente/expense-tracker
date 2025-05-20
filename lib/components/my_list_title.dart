d88ef8d52a46ff82ba247307cc94dbf6f9f8a7c3
Thales <thalesvicentebem10@hotmail.com>
2025-05-20 11:43:18 -0300

    `flutter_slidable` to enable swipe actions (edit and delete) on list items in `MyListTitle`

--- lib/components/my_list_title.dart
+++ lib/components/my_list_title.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class MyListTitle extends StatelessWidget {
   final String title;
   final String trailing;
+  final void Function(BuildContext)? onEditPressed;
+  final void Function(BuildContext)? onDeletePressed;
 
-  const MyListTitle({super.key, required this.title, required this.trailing});
+  const MyListTitle({
+    super.key,
+    required this.title,
+    required this.trailing,
+    this.onEditPressed,
+    this.onDeletePressed,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(title),
-      trailing: Text(trailing),
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const StretchMotion(),
+        children: [
+          // TODO settings option
+          SlidableAction(
+              onPressed: onEditPressed,
+              icon: Icons.settings
+          ),
+
+          // TODO delete option
+          SlidableAction(
+              onPressed: onEditPressed,
+              icon: Icons.delete,
+          ),
+        ],
+      ),
+      child: ListTile(title: Text(title), trailing: Text(trailing)),
     );
   }
 }
