ded2407cf4558bb2eb2e1b897934263386398d9a
Thales <thalesvicentebem10@hotmail.com>
2025-05-20 17:32:37 -0300

    bar graph widget to display monthly summaries.

--- lib/bar graph/bar_graph.dart
+++ lib/bar graph/bar_graph.dart
@@ -21,23 +21,99 @@ class _MyBarGraphState extends State<MyBarGraph> {
   List<IndividualBar> barData = [];
 
   // TODO initialize bar data - user our monthly summary to create a list of bars
-  void initializeBarData(){
+  void initializeBarData() {
     barData = List.generate(
-        widget.monthlySummary.length,
-        (index) => IndividualBar(
-          x: index,
-          y: widget.monthlySummary[index],
-        )
+      widget.monthlySummary.length,
+      (index) => IndividualBar(x: index, y: widget.monthlySummary[index]),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    //TODO initialize upon build
+    initializeBarData();
+
     return BarChart(
       BarChartData(
         minY: 0,
         maxY: 100,
-      )
+        gridData: const FlGridData(show: false),
+        borderData: FlBorderData(show: false),
+        titlesData: const FlTitlesData(
+          show: true,
+          topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: getBottomTitles,
+            ),
+          ),
+        ),
+        barGroups: barData.map(
+            (data) => BarChartGroupData(
+                x: data.x,
+              barRods: [
+                BarChartRodData(toY: data.y),
+              ],
+            ),
+        ).toList()
+      ),
     );
   }
+}
+
+// TODO B O T T O M - T I T L E S
+Widget getBottomTitles(double value, TitleMeta meta) {
+  const textstyle = TextStyle(
+    color: Colors.grey,
+    fontWeight: FontWeight.bold,
+    fontSize: 14,
+  );
+
+  String text;
+  switch (value.toInt()) {
+    case 0:
+      text = 'J';
+      break;
+    case 1:
+      text = 'F';
+      break;
+    case 2:
+      text = 'M';
+      break;
+    case 3:
+      text = 'A';
+      break;
+    case 4:
+      text = 'M';
+      break;
+    case 5:
+      text = 'J';
+      break;
+    case 6:
+      text = 'J';
+      break;
+    case 7:
+      text = 'A';
+      break;
+    case 8:
+      text = 'S';
+      break;
+    case 9:
+      text = 'O';
+      break;
+    case 10:
+      text = 'N';
+      break;
+    case 11:
+      text = 'D';
+      break;
+    default:
+      text = '';
+      break;
+  }
+
+  return SideTitleWidget(child: Text(text, style: textstyle,), meta: meta);
 }
